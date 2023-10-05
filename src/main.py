84d9ab80be07a1754367089a76507898ad7bae0c
TASCer <todd@tascs.net>
2023-10-05 12:48:33 -0700

    updated logging

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# TODO hoa seems to work, tascssolutions not so much
 import datetime as dt
 import logging
 import my_secrets
@@ -25,17 +26,18 @@ root_logger.addHandler(fh)
 new_line = '\n'
 
 
-# hoa seems to wotk, tascs not so much
 def process_logs():
-	# remove = string.punctuation
 	log_entries = []
-	with open('../test/hoaOct-2023') as logs:
+	with open('../test/tascsolutions_sslOct-2023') as logs:
 		for log in logs:
 			basic = log.split('" "')[0]
+			basic_info = basic.split("- - ")[1]
+			ts = basic_info.split(']')[0][1:]
+			date, time = ts.split(':', 1)
 			ip = basic.split("- - ")[0]
 			ip = ip.rstrip()
-			# skip cron jobs on my server
-			if ip == f'{my_secrets.home_ip}':
+
+			if ip == f'{my_secrets.home_ip}':  # skip cron jobs on my server
 				continue
 
 			client = re.findall("\((.*?)\)", log)
@@ -58,17 +60,6 @@ def process_logs():
 			action_http_ver = action_info[2]
 
 			agent_info = log.split('" "')[1]
-
-			# query_ip = IPWhois(ip)
-			# res = query_ip.lookup_rdap()
-
-			country = "TEST" # res['asn_country_code']
-
-			basic_info = basic.split("- - ")[1]
-
-			ts = basic_info.split(']')[0][1:]
-			date, time = ts.split(':', 1)
-
 			agent_list = agent_info.split(' ')
 			agent_name = agent_list[0]
 
@@ -80,20 +71,19 @@ def process_logs():
 			# print(f"ip: {ip}{new_line}country: {country}{new_line}date: {date}{new_line}time: {time}{new_line}client_os: {client_os}{new_line}client_format: {client_format}{new_line}"
 			# 	  f"action verb: {action_verb}{new_line}action_file: {action_file}{new_line}action_http_ver: {action_http_ver}{new_line}agent_name: {agent_name}"
 			# 	  f"{new_line}agent_referer_ip: {agent_referer_ip}{new_line}agent_referer_url: {agent_referer_url}{new_line}")
-				  # f"action verb: {action_verb}{new_line}action_file: {action_file}{new_line}action_http_ver: {action_http_ver}")
+			# f"action verb: {action_verb}{new_line}action_file: {action_file}{new_line}action_http_ver: {action_http_ver}")
 			# print(f"{ip}\t\t {agent_name}")
 			# print("-------------------------------------------------------")
 			log_entries.append(ip)
-	# with open engine as conn, conn.begin
-
 
 	return log_entries
 
 
 if __name__ == '__main__':
+	logger: Logger = logging.getLogger(__name__)
 	processed_logs: list = process_logs()
-	print(f"HITS: {len(processed_logs)}")
+	logger.info(f"HITS: {len(processed_logs)}")
 	unique_processed_logs: set = set(processed_logs)
-	print(f"Unique HITS: {len(unique_processed_logs)}")
+	logger.info(f"Unique HITS: {len(unique_processed_logs)}")
 	get_country_name.find(unique_processed_logs)
 	# update_country_name.update(unique_processed_logs)
