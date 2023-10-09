b9e8e48037ae5de3d52baa14b3b2c70e6aec8985
TASCer <todd@tascs.net>
2023-10-09 13:47:29 -0700

    just add unique ips to table

--- src/update_lookup_table.py
+++ src/update_lookup_table.py
@@ -1,11 +1,11 @@
 import datetime as dt
-import ipwhois
+# import ipwhois
 import logging
 from src import my_secrets
 
 from datetime import datetime
-from ipwhois.utils import get_countries
-from ipwhois import IPWhois
+# from ipwhois.utils import get_countries
+# from ipwhois import IPWhois
 from logging import Logger
 from sqlalchemy.engine import Engine, CursorResult
 from sqlalchemy import exc, create_engine, text
@@ -14,11 +14,11 @@ from typing import Optional, Any
 now: datetime = dt.datetime.now()
 todays_date: str = now.strftime('%D').replace('/', '-')
 
-COUNTRIES = get_countries()
+# COUNTRIES = get_countries()
 
 
 def update(ips: list) -> object:
-    """Updates lookup table with unique ips from ALPHA-2 to full country name"""
+    """Takes in list of unique source ip addresses and ipdates lookup table to have country name added later"""
     logger: Logger = logging.getLogger(__name__)
     try:
         engine: Engine = create_engine(f"mysql+pymysql://{my_secrets.dbuser}:{my_secrets.dbpass}@{my_secrets.dbhost}/{my_secrets.dbname}")
@@ -30,54 +30,4 @@ def update(ips: list) -> object:
     # ISSUE CHECK IF ALREADY FOUND COUNTRY NAME
     with engine.connect() as conn, conn.begin():
         for ip in ips:
-            res = conn.execute(text('''select country from lookup where source = '{ip}';'''))
-
-            c = res.first()
-            print(len(res))
-            # try:
-            #     obj: IPWhois = ipwhois.IPWhois(ip, timeout=10)
-            #     result: dict = obj.lookup_rdap()
-            #     asn_alpha2: str = result['asn_country_code']
-            #
-            #     if asn_alpha2 is None or asn_alpha2 == '':
-            #         logger.warning(f"{ip} had no alpha2 code, setting country name to 'unknown'")
-            #         asn_alpha2: str = 'unknown'
-            #         conn.execute(f'''update lookup SET country = '{asn_alpha2}' WHERE SOURCE = '{ip}';''')
-            #         continue
-            #
-            #     elif asn_alpha2.islower():
-            #         asn_alpha2: str = asn_alpha2.upper()
-            #         logger.warning(f'RDAP responded with lowercase country for {ip}, should be upper')
-            #
-            #     else:
-            #         country_name: Optional[Any] = COUNTRIES.get(asn_alpha2)
-            #
-            # except (UnboundLocalError, ValueError, AttributeError, ipwhois.BaseIpwhoisException, ipwhois.ASNLookupError,
-            #         ipwhois.ASNParseError, ipwhois.ASNOriginLookupError, ipwhois.ASNRegistryError,
-            #         ipwhois.HostLookupError, ipwhois.HTTPLookupError) as e:
-            #
-            #     result = None
-            #     error: str = str(e).split('http:')[0]
-            #     logger.error(f"{error} {ip}")
-            #
-            # conn.execute(text(f'''INSERT IGNORE INTO `bluehost-logs`.`lookup` VALUES('{ip}', '{country_name}');'''))
-
-        #         conn.execute(f'''update lookup SET country = '{error}' WHERE SOURCE = '{ip}';''')
-        #         continue
-        #
-
-        #
-
-        #
-        #     if not country_name:
-        #         logger.warning("Country Name not found in COUNTRIES, setting it to alpha-2")
-        #         conn.execute(f'''update lookup SET country = '{asn_alpha2}' WHERE SOURCE = '{ip}';''')
-        #         continue
-        #
-        #     elif "'" in country_name:
-        #         country_name = country_name.replace("'", "''")
-        #         logger.warning(f"Apostrophe found in {country_name}")
-        #         conn.execute(f'''update lookup SET country = '{country_name}' WHERE SOURCE = '{ip}';''')
-        #
-        #     else:
-        #         conn.execute(f'''update lookup SET country = '{country_name}' WHERE SOURCE = '{ip}';''')
+            ins_sql = conn.execute(text(f'''INSERT IGNORE into `bluehost-logs`.lookup values('{ip}', 'tbd');'''))
