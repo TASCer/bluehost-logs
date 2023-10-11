68e0ffaa988cc76988316a966c271f750bc7d570
TASCer <todd@tascs.net>
2023-10-11 14:37:32 -0700

    cleaned

--- src/update_activity_table.py
+++ src/update_activity_table.py
@@ -1,13 +1,13 @@
 import datetime as dt
-import ipwhois
+# import ipwhois
 import logging
-import time
+# import time
 
 from dateutil.parser import *
 from datetime import datetime
 from src import my_secrets
 from ipwhois.utils import get_countries
-from ipwhois import IPWhois
+# from ipwhois import IPWhois
 from logging import Logger
 from sqlalchemy.engine import Engine, CursorResult
 from sqlalchemy import exc, create_engine, text
@@ -31,13 +31,13 @@ def update(log_entries: list) -> object:
         exit()
 
     with engine.connect() as conn, conn.begin():
-        for ip, ts, action, file, type in log_entries:
-            ts_orig = ts.replace(':', ' ', 1)
+        for ip, ts, action, file, conn_type in log_entries:
+            ts_orig = ts.replace(":", " ", 1)
             ts_split = ts_orig.split(" ", 2)
             ts = ' '.join(ts_split[0:2])
             ts_parsed = parse(ts)
 
             try:
-                conn.execute(text(f'''INSERT INTO `bluehost-logs`.`activity` VALUES(id, '{ip}', '{action}', '{file}', '{type}', '{ts_parsed}');'''))
+                conn.execute(text(f'''INSERT INTO `bluehost-logs`.`activity` VALUES(id, '{ip}', '{action}', '{file}', '{conn_type}', '{ts_parsed}');'''))
             except exc.SQLAlchemyError as e:
                 logger.error(str(e))
