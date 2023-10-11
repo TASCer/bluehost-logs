901ab62a956ba4f1fac086a1635dfd5bcce18ff4
TASCer <todd@tascs.net>
2023-10-11 12:15:40 -0700

    cleaned

--- src/update_lookup_table.py
+++ src/update_lookup_table.py
@@ -1,23 +1,17 @@
 import datetime as dt
-# import ipwhois
 import logging
 from src import my_secrets
 
 from datetime import datetime
-# from ipwhois.utils import get_countries
-# from ipwhois import IPWhois
 from logging import Logger
 from sqlalchemy.engine import Engine, CursorResult
 from sqlalchemy import exc, create_engine, text
-from typing import Optional, Any
 
 now: datetime = dt.datetime.now()
 todays_date: str = now.strftime('%D').replace('/', '-')
 
-# COUNTRIES = get_countries()
 
-
-def update(ips: list) -> object:
+def update(unique_ips: list) -> object:
     """Takes in list of unique source ip addresses and ipdates lookup table to have country name added later"""
     logger: Logger = logging.getLogger(__name__)
     try:
@@ -27,7 +21,7 @@ def update(ips: list) -> object:
         logger.critical(str(e))
         engine = None
         exit()
-    # ISSUE CHECK IF ALREADY FOUND COUNTRY NAME
+
     with engine.connect() as conn, conn.begin():
-        for ip in ips:
+        for ip in unique_ips:
             ins_sql = conn.execute(text(f'''INSERT IGNORE into `bluehost-logs`.lookup values('{ip}', '', '');'''))
