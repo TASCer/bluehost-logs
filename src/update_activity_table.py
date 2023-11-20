1c139a1b7b8d31b0a6f37013eecabf878aa4b0fc
TASCer <todd@tascs.net>
2023-11-20 09:52:37 -0700

    seperate timestamp parse func

--- src/update_activity_table.py
+++ src/update_activity_table.py
@@ -19,6 +19,15 @@ todays_date: str = now.strftime('%D').replace('/', '-')
 COUNTRIES = get_countries()
 
 
+def parse_timestamp(ts: str) -> datetime:
+    ts = ts.replace(":", " ", 1)
+    ts_split = ts.split(" ", 2)
+    ts = ' '.join(ts_split[0:2])
+    ts_parsed = parse(ts)
+
+    return ts_parsed
+
+
 def update(log_entries: list) -> object:
     """Updates lookup table with unique ips from ALPHA-2 to full country name"""
     logger: Logger = logging.getLogger(__name__)
@@ -32,10 +41,7 @@ def update(log_entries: list) -> object:
 
     with engine.connect() as conn, conn.begin():
         for ip, ts, action, file, conn_type, ref_url, ref_ip, action_code, action_size in log_entries:
-            ts_orig = ts.replace(":", " ", 1)
-            ts_split = ts_orig.split(" ", 2)
-            ts = ' '.join(ts_split[0:2])
-            ts_parsed = parse(ts)
+            ts_parsed = parse_timestamp(ts)
 
             try:
                 conn.execute(text(f'''INSERT INTO `bluehost-logs`.`activity` VALUES(id, '{ip}', '{action}', '{file}', '{conn_type}', '{action_code}', '{action_size}', '{ref_url}', '{ref_ip}', '{ts_parsed}');'''))
