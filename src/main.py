b356bfc315f04eb8638770bfe7cb3cec13778d59
TASCer <todd@tascs.net>
2023-12-08 14:13:18 -0700

    reworked

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 # TODO THINGS SEEM TO WORK, DO MORE TESTING and fully populate lookup country, create db backups. Start fresh in 2024
 import datetime as dt
 import db_checks
-
 import logging
 import my_secrets
+import parse_logs
 import re
 import update_activity_table
 import update_lookup_country
@@ -28,90 +28,16 @@ root_logger.addHandler(fh)
 
 logger: Logger = logging.getLogger(__name__)
 
-TESTING: bool = False
-
-def process_logs():
-	log_entries: list = []
-	sources: list = []
-	with open('../input/tascsolutions_sslOct-2023') as logs:
-		for log in logs:
-			basic = log.split('" "')[0]
-			ip = basic.split("- - ")[0]
-			ip = ip.rstrip()
-
-			# skip parsing system cron jobs on bluehost server or activity from my home office when not testing
-			if not TESTING and ip == f'{my_secrets.bh_home_ip}' or ip == f'{my_secrets.home_ip}':
-					continue
-
-			basic_info = basic.split("- - ")[1]
-			server_timestamp: str = basic_info.split(']')[0][1:]
-
-			try:
-				action1 = basic_info.split('"')[1]
-				action, action_file, action_http_ver = action1.split(' ')
-
-			except ValueError as e:
-				logger.error(f"{ip}--{e}")
-				continue
-
-			if len(action_file) >= 120:
-				action_list = action_file.split('?')
-				action_file1 = action_list[0]
-				action_file2 = action_list[1][:80]
-				action_file = action_file1+action_file2+' *TRUNCATED*'
-				logger.warning(f"{ip} had too long requested file name, truncated")
-
-			try:
-				action2 = basic_info.split('"')[2].strip()
-				action_code, action_size = action2.split(' ')
-
-			except ValueError as e:
-				logger.error(e, "Possible bot, check logs")
-				continue
-
-			agent_info = log.split('" "')[1]
-			agent_list = agent_info.split(' ')
-			agent_name = agent_list[0].replace('"', '')
-
-			if agent_name.startswith('-'):
-				agent_name = "NA"
-
-			referer_ip = agent_list[-1].strip()
-			referer_url = agent_list[-2]
-
-			# finds everything between all(    )
-			client: list = re.findall("\((.*?)\)", log)
-
-			if not client:
-				client_os, client_format = 2 * ('NA',)
-
-			elif len(client) == 1:
-				client_format = 'NA'
-				client_os = client[0]
-				client_os = client_os.replace(';', '')
-
-			else:
-				client_os = client[0]
-				client_os = client_os.replace(';', '')
-				client_format = client[1]
-
-			sources.append(ip)
-			log_entries.append((ip, server_timestamp, action, action_file, action_http_ver, referer_url, referer_ip, action_code, action_size, agent_name, client_os))
-
-	return sources, log_entries
-
 
 if __name__ == '__main__':
 	logger.info("Checking RDBMS Availability")
 	have_database: bool = db_checks.schema()
 	have_tables: bool = db_checks.tables()
-
 	if have_database and have_tables:
 		logger.info("RDBMS is available and ready")
 	else:
 		logger.error(f"RDBMS IS NOT OPERATIONAL: RDBMS: {have_database} / TABLES: {have_tables}")
-
-	ips, processed_logs = process_logs()
+	ips, processed_logs = parse_logs.process()
 	unique_sources: set = set(ips)
 	logger.info(f"HITS: {len(processed_logs)} Unique HITS: {len(unique_sources)}")
 	update_lookup_table.update(unique_sources)
