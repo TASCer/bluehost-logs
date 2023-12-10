d27d8f80c53e8b2672b8be3aced5b34bf81bed76
TASCer <todd@tascs.net>
2023-12-10 11:18:22 -0700

    renamed col client

--- src/inserts_activity_table.py
+++ src/inserts_activity_table.py
@@ -40,10 +40,10 @@ def update(log_entries: list) -> object:
         exit()
 
     with engine.connect() as conn, conn.begin():
-        for ip, ts, action, file, conn_type, ref_url, ref_ip, action_code, action_size, agent_name, client_os in log_entries:
+        for ip, ts, action, file, conn_type, ref_url, ref_ip, action_code, action_size, agent_name, client in log_entries:
             ts_parsed = parse_timestamp(ts)
 
             try:
-                conn.execute(text(f'''INSERT IGNORE INTO {LOGS} VALUES('{id}', '{ts_parsed}', '{ip}', '{client_os}', '{agent_name}', '{action}', '{file}', '{conn_type}', '{action_code}', '{action_size}', '{ref_url}', '{ref_ip}');'''))
+                conn.execute(text(f'''INSERT IGNORE INTO {LOGS} VALUES('{id}', '{ts_parsed}', '{ip}', '{client}', '{agent_name}', '{action}', '{file}', '{conn_type}', '{action_code}', '{action_size}', '{ref_url}', '{ref_ip}');'''))
             except (exc.SQLAlchemyError, exc.ProgrammingError, exc.DataError) as e:
                 logger.error(e)
