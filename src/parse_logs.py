393edf3482feeece087f0ec0fe47ff2fcde32fe6
TASCer <todd@tascs.net>
2023-12-15 15:05:04 -0700

    + namedtuple class

--- src/parse_logs.py
+++ src/parse_logs.py
@@ -2,12 +2,26 @@ import logging
 import my_secrets
 import re
 
+from collections import namedtuple
 from logging import Logger
-
-
+from typing import NamedTuple
 logger: Logger = logging.getLogger(__name__)
 
-WEBLOG_FILE: str = "../tests/test_tascsMay-2023-ISSUES"
+WEBLOG_FILE: str = "../tests/test_hoa_sslOct-2023"
+
+
+class LogEntry(NamedTuple):
+	server_timestamp: str
+	SOURCE: str
+	CLIENT: str
+	AGENT: str
+	ACTION: str
+	FILE: str
+	TYPE: str
+	RES_CODE: str
+	SIZE: int
+	REF_URL: str
+	REF_IP: str
 
 
 def process():
@@ -17,10 +31,10 @@ def process():
 		for log in logs:
 			basic = log.split('" "')[0]
 			ip = basic.split("- - ")[0]
-			ip = ip.rstrip()
+			SOURCE = ip.rstrip()
 
 			# skip parsing system cron jobs on bluehost server or activity from my home office when not testing
-			if ip == f'{my_secrets.bh_ip}' or ip == f'{my_secrets.home_ip}':
+			if SOURCE == f'{my_secrets.bh_ip}' or SOURCE == f'{my_secrets.home_ip}':
 				continue
 
 			basic_info = basic.split("- - ")[1]
@@ -28,22 +42,22 @@ def process():
 
 			try:
 				action1 = basic_info.split('"')[1]
-				action, action_file, action_http_ver = action1.split(' ')
+				ACTION, FILE, TYPE = action1.split(' ')
 
 			except ValueError as e:
 				logger.error(f"{ip}--{e}")
 				continue
 
-			if len(action_file) >= 120:
-				action_list = action_file.split('?')
+			if len(FILE) >= 120:
+				action_list = FILE.split('?')
 				action_file1 = action_list[0]
 				action_file2 = action_list[1][:80]
-				action_file = action_file1+action_file2+' *TRUNCATED*'
-				logger.warning(f"{ip} had too long requested file name, truncated")
+				FILE = action_file1+action_file2+' *TRUNCATED*'
+				logger.warning(f"{SOURCE} had too long requested file name, truncated")
 
 			try:
 				action2 = basic_info.split('"')[2].strip()
-				action_code, action_size = action2.split(' ')
+				RES_CODE, SIZE = action2.split(' ')
 
 			except ValueError as e:
 				logger.error("Possible bot, check logs")
@@ -51,31 +65,32 @@ def process():
 
 			agent_info = log.split('" "')[1]
 			agent_list = agent_info.split(' ')
-			agent_name = agent_list[0].replace('"', '')
+			AGENT = agent_list[0].replace('"', '')
 
-			if agent_name.startswith('-'):
-				agent_name = "NA"
+			if AGENT.startswith('-'):
+				AGENT = "NA"
 
-			referer_ip = agent_list[-1].strip()
-			referer_url = agent_list[-2]
+			REF_IP = agent_list[-1].strip()
+			REF_URL = agent_list[-2]
 
 			# finds everything between all(    )
 			client: list = re.findall("\((.*?)\)", log)
 
 			if not client:
-				client_os, client_format = 2 * ('NA',)
+				CLIENT, client_format = 2 * ('NA',)
 
 			elif len(client) == 1:
 				client_format = 'NA'
 				client_os = client[0]
-				client_os = client_os.replace(';', '')
+				CLIENT = client_os.replace(';', '')
 
 			else:
 				client_os = client[0]
-				client_os = client_os.replace(';', '')
+				CLIENT = client_os.replace(';', '')
 				client_format = client[1]
 
-			sources.append(ip)
-			log_entries.append((ip, server_timestamp, action, action_file, action_http_ver, referer_url, referer_ip, action_code, action_size, agent_name, client_os))
+			sources.append(SOURCE)
+			entry = LogEntry(SOURCE, server_timestamp, ACTION, FILE, TYPE, REF_URL, REF_IP, RES_CODE, SIZE, AGENT, CLIENT)
+			log_entries.append(entry)
 
 	return sources, log_entries
