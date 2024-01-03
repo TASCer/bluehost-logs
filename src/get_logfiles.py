421b83e4ba39de06a2bb2ed6f3f1aa857b6df4ff
TASCer <todd@tascs.net>
2024-01-03 15:31:36 -0700

    cleaned

--- src/get_logfiles.py
+++ src/get_logfiles.py
@@ -3,7 +3,6 @@ import gzip
 import logging
 import my_secrets
 import os
-import shutil
 import platform
 
 from datetime import datetime
@@ -17,32 +16,32 @@ month_num = now.month
 month = now.strftime('%b')
 
 
-
-
 def secure_copy(paths: list[str]) -> None:
 	"""
 	Takes in a list of string paths
 	"""
 	for path in paths:
 		remote_zipped_filename = path+month+'-'+str(now.year)+'.gz'
-		local_unzipped_filename = remote_zipped_filename.split()
-		local_filename = path+month+'-'+str(now.year)
-		local_filename = local_filename.split("/")[1]
-		print(remote_zipped_filename, local_filename)
+
+		local_zipped_filename = path + month + '-' + str(now.year)
+		local_zipped_filename = local_zipped_filename.split("/")[1]
+		local_unzipped_filename = remote_zipped_filename.split("/")[1]
 
 		if not platform.system() == 'Windows':
 			try:
-				os.system(f'scp {path} {my_secrets.local_dir}')
-				logger.info(f"{path} {my_secrets.local_dir} retrieved from bh server")
+				os.system(f'scp {path} {my_secrets.local_zipped_path}')
+				logger.info(f"{path} {my_secrets.local_zipped_path} retrieved from bh server")
 			except (BaseException, FileNotFoundError) as e:
 				logger.critical(f"{path} LOG NOT RETRIEVED. Investigate")
 		else:
 			try:
 				os.system(f'pscp {my_secrets.user}@{my_secrets.bh_ip}:{remote_zipped_filename} {my_secrets.local_zipped_path}')
-				logger.info(f"{path} {local_filename} retrieved from bh server")
+				logger.info(f"{path} {remote_zipped_filename} retrieved from bh server")
 			except (BaseException, FileNotFoundError) as e:
 				logger.critical(f"{e}")
 
-		# with gzip.open(f'{my_secrets.local_zipped_path}{local_filename}.gz') as zipped_file:
-		# 	with open(f"{local_unzipped_filename}{local_filename}", 'wb') as unzipped_file:
-		# 		unzipped_file.write(zipped_file.read())
+		print(f'{my_secrets.local_zipped_path}{local_zipped_filename}')
+
+		with gzip.open(f'{my_secrets.local_zipped_path}{local_zipped_filename}.gz') as zipped_file:
+			with open(f"{my_secrets.local_unzipped_path}{local_unzipped_filename}", 'wb') as unzipped_file:
+				unzipped_file.write(zipped_file.read())
