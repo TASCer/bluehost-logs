4ff9f047d5aebd17398ecffb941c4d880594d24d
TASCer <todd@tascs.net>
2024-01-03 14:48:50 -0700

    working, need to unzip

--- src/get_logfiles.py
+++ src/get_logfiles.py
@@ -19,39 +19,30 @@ month = now.strftime('%b')
 
 
 
-
-
-
-
 def secure_copy(paths: list[str]) -> None:
 	"""
 	Takes in a list of string paths
 	"""
 	for path in paths:
-		filename = path+month+'-'+str(now.year)
-		print(filename)
+		remote_zipped_filename = path+month+'-'+str(now.year)+'.gz'
+		local_unzipped_filename = remote_zipped_filename.split()
+		local_filename = path+month+'-'+str(now.year)
+		local_filename = local_filename.split("/")[1]
+		print(remote_zipped_filename, local_filename)
 
-		# if not platform.system() == 'Windows':
-		# 	try:
-		# 		os.system(f'scp {path} {my_secrets.local_dir}')
-		# 		logger.info(f"{path} {my_secrets.local_dir} retrieved from bh server")
-		# 	except (BaseException, FileNotFoundError) as e:
-		# 		logger.critical(f"{path} LOG NOT RETRIEVED. Investigate")
-		# else:
-		# 	try:
-		# 		os.system(f'pscp {my_secrets.user}@{my_secrets.bh_ip}:{filename}.gz ../input/zipped/')
-		# 		logger.info(f"{path} {my_secrets.local_dir} retrieved from bh server")
-		# 	except (BaseException, FileNotFoundError) as e:
-		# 		logger.critical(f"{path} LOG NOT RETRIEVED. Investigate")
+		if not platform.system() == 'Windows':
+			try:
+				os.system(f'scp {path} {my_secrets.local_dir}')
+				logger.info(f"{path} {my_secrets.local_dir} retrieved from bh server")
+			except (BaseException, FileNotFoundError) as e:
+				logger.critical(f"{path} LOG NOT RETRIEVED. Investigate")
+		else:
+			try:
+				os.system(f'pscp {my_secrets.user}@{my_secrets.bh_ip}:{remote_zipped_filename} {my_secrets.local_zipped_path}')
+				logger.info(f"{path} {local_filename} retrieved from bh server")
+			except (BaseException, FileNotFoundError) as e:
+				logger.critical(f"{e}")
 
-		with gzip.open('../input/zipped/roadspies.cag.bis.mybluehost.me-ssl_log-Jan-2024.gz') as zipped_file:
-			with open(f"../input/roadspies", 'wb') as unzipped_file:
-				unzipped_file.write(zipped_file.read())
-
-		# else:
-		# 	try:
-		# 		shutil.copy(path, my_secrets.local_dir)
-		#
-		# 	except (IOError, FileNotFoundError) as e:
-		# 		logger.error(e)
-
+		# with gzip.open(f'{my_secrets.local_zipped_path}{local_filename}.gz') as zipped_file:
+		# 	with open(f"{local_unzipped_filename}{local_filename}", 'wb') as unzipped_file:
+		# 		unzipped_file.write(zipped_file.read())
