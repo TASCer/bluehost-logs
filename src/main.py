dc67d740e23ba6dc42423d5ce8dbf3b517ce9d1f
TASCer <todd@tascs.net>
2024-01-05 09:11:22 -0700

    cleaned + hist log path

--- src/main.py
+++ src/main.py
@@ -5,12 +5,10 @@ import get_logfiles
 import logging
 import my_secrets
 import parse_logs
-import re
 import inserts_activity_table
 import update_lookup_country
 import inserts_lookup_table
 
-# from collections import namedtuple
 from logging import Logger, Formatter
 
 now: dt = dt.date.today()
@@ -29,23 +27,26 @@ root_logger.addHandler(fh)
 
 logger: Logger = logging.getLogger(__name__)
 
-tascs_log_path = my_secrets.tascs_logs_zipped
+tascs_logs_path = my_secrets.tascs_logs_zipped
 hoa_logs_path = my_secrets.hoa_logs_zipped
-roadspies_log_path = my_secrets.roadspies_logs_zipped
+roadspies_logs_path = my_secrets.roadspies_logs_zipped
+tascs_logs_historical_path = my_secrets.tascs_logs_historical_zipped
 
-remote_log_file_paths = [roadspies_log_path, tascs_log_path, hoa_logs_path]
+remote_log_file_paths = [tascs_logs_historical_path] # roadspies_logs_path, tascs_logs_historical_path, tascs_logs_path, hoa_logs_path]
 
 
 if __name__ == '__main__':
 	logger.info("Checking RDBMS Availability")
+	month_num: int = 2
+	year: str = 2020
 	have_database: bool = db_checks.schema()
 	have_tables: bool = db_checks.tables()
 	if have_database and have_tables:
 		logger.info("RDBMS is available and ready")
 	else:
 		logger.error(f"RDBMS IS NOT OPERATIONAL: RDBMS: {have_database} / TABLES: {have_tables}")
-	get_logfiles.secure_copy(remote_log_file_paths)
-	ips, processed_logs = parse_logs.process(WEBLOG_FILE)
+	get_logfiles.secure_copy(remote_log_file_paths, month_num, year)
+	ips, processed_logs = parse_logs.process()
 	unique_sources: set = set(ips)
 	inserts_lookup_table.update(unique_sources)
 	update_lookup_country.get(unique_sources)
