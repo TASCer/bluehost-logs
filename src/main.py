26db8391af676a8b5474cf5a6a95aa53b3db7ba3
TASCer <todd@tascs.net>
2024-01-09 10:20:40 -0700

    logging changes

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ hoa_logs_path = my_secrets.hoa_logs_zipped
 roadspies_logs_path = my_secrets.roadspies_logs_zipped
 tascs_logs_historical_path = my_secrets.tascs_logs_historical_zipped
 
-remote_log_file_paths = [roadspies_logs_path, tascs_logs_path, hoa_logs_path]
+remote_log_file_paths = [tascs_logs_path, hoa_logs_path, roadspies_logs_path]
 # historical_remote_log_file_paths = [tascs_logs_historical_path}
 
 if __name__ == '__main__':
@@ -47,9 +47,11 @@ if __name__ == '__main__':
 	else:
 		logger.error(f"RDBMS IS NOT OPERATIONAL: RDBMS: {have_database} / TABLES: {have_tables}")
 
+	logger.info("***** STARTING LOG PROCESSING *****")
 	processed_log_path: list[str] = get_logfiles.secure_copy(remote_log_file_paths, month_num, year)
 	ips, processed_logs = parse_logs.process(processed_log_path)
 	unique_sources: set = set(ips)
 	insert_unique_sources.update(unique_sources)
 	update_sources_country.get(unique_sources)
 	insert_activity.update(processed_logs)
+	logger.info("***** COMPLETED WEB LOG PROCESSING *****")
