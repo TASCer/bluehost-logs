0ea2eb04bcdad07d4551fb62be5c2673e326a1ac
TASCer <todd@tascs.net>
2024-01-18 10:40:40 -0700

    initial

---
+++ assets/sample_my_secrets.py
@@ -0,0 +1,20 @@
+# HOST IP ADXs
+bh_ip = "x.x.x.x"
+home_ip = "x.x.x.x"
+# SECURE ACCESS TO FILES ON BH SERVER
+user = "your_user"
+password = "your_password"
+key_path = r"C:\path\to\your\file.ppk"
+# ACCESS TO OPS BLUEHOST-WEBLOGS DB
+dbuser = "your_dbuser"
+dbpass = "your_dbpassword"
+dbhost = "your_dbhostname"
+dbname = "your_dbname"
+# SERVER LOG FILES
+tascs_logs_historical_zipped = "logs/tascsolutions.com-"
+tascs_logs_zipped = "logs/cag.bis.mybluehost.me-ssl_log-"
+hoa_logs_zipped = "logs/test.url.net-ssl_log-"
+roadspies_logs_zipped = 'logs/roadspies.cag.bis.mybluehost.me-ssl_log-'
+# LOCAL LOG FILES
+local_zipped_path = "../input/zipped_logfiles/"
+local_unzipped_path = "../output/unzipped_logfiles/"
