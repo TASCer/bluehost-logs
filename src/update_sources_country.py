392fcdde464039d28928e1c036121c785175bff1
TASCer <todd@tascs.net>
2024-05-12 12:53:55 -0700

    formatted, cleaned

--- src/update_sources_country.py
+++ src/update_sources_country.py
@@ -21,70 +21,71 @@ SOURCES = 'sources'
 COUNTRIES = get_countries()
 
 
-def get(unique_ips: list):
+def get():
     """Updates lookup table SOURCE entries with full country name and ASN Description"""
     logger: Logger = logging.getLogger(__name__)
-    country_found: list = []
-    country_not_found: list = []
 
     try:
-        engine: Engine = create_engine(f"mysql+pymysql://{my_secrets.dbuser}:{my_secrets.dbpass}@{my_secrets.dbhost}/{my_secrets.dbname}")
+        engine: Engine = create_engine(
+            f"mysql+pymysql://{my_secrets.dbuser}:{my_secrets.dbpass}@{my_secrets.dbhost}/{my_secrets.dbname}")
 
     except exc.SQLAlchemyError as e:
         logger.critical(str(e))
-        engine = None
         exit()
 
     with engine.connect() as conn, conn.begin():
-            logger.info("Updating lookup table with source country name and description via IPWhois")
-            errors = 0
+        logger.info("Updating lookup table with source country name and description via IPWhois")
+        errors = 0
+        try:
+            sql_no_country: CursorResult = conn.execute(
+                text(f'''SELECT * from {SOURCES} WHERE COUNTRY = '' or COUNTRY is null;'''))
+            no_country: list = [i for i in sql_no_country]
+        except exc.SQLAlchemyError as e:
+            logger.warning(str(e))
+
+        for ip, country, code, desc in no_country:
             try:
-                sql_no_country: CursorResult = conn.execute(text(f'''SELECT * from {SOURCES} WHERE COUNTRY = '' or COUNTRY is null;'''))
-                no_country: list = [i for i in sql_no_country]
-            except exc.SQLAlchemyError as e:
-                logger.warning(str(e))
+                obj: IPWhois = ipwhois.IPWhois(ip, timeout=10)
+                result: dict = obj.lookup_rdap()
 
-            for ip, country, code, desc in no_country:
-                try:
-                    obj: IPWhois = ipwhois.IPWhois(ip, timeout=10)
-                    result: dict = obj.lookup_rdap()
+            except (UnboundLocalError, ValueError, AttributeError, ipwhois.BaseIpwhoisException, ipwhois.ASNLookupError,
+                    ipwhois.ASNParseError, ipwhois.ASNOriginLookupError, ipwhois.ASNRegistryError,
+                    ipwhois.HostLookupError, ipwhois.HTTPLookupError) as e:
+                logger.exception(e)
+                errors += 1
+                continue
 
-                except (UnboundLocalError, ValueError, AttributeError, ipwhois.BaseIpwhoisException, ipwhois.ASNLookupError,
-                        ipwhois.ASNParseError, ipwhois.ASNOriginLookupError, ipwhois.ASNRegistryError,
-                        ipwhois.HostLookupError, ipwhois.HTTPLookupError) as e:
-                    errors += 1
-                    continue
+            asn_description: str = result['asn_description']
 
-                asn_description: str = result['asn_description']
+            if asn_description == "NA" or asn_description is None:
+                asn_description = "NA"
+            else:
+                asn_description = asn_description.rsplit(',')[0]
 
-                if asn_description == "NA" or asn_description is None:
-                    asn_description = "NA"
-                else:
-                    asn_description = asn_description.rsplit(',')[0]
+            if result['asn_country_code'] is None:
+                logger.warning(f"{ip} had no alpha2 code, setting country name to '00'")
+                asn_alpha2: str = '00'
+                conn.execute(text(f'''update {SOURCES} SET country = '{asn_alpha2}' WHERE SOURCE = '{ip}';'''))
+                continue
 
-                if result['asn_country_code'] is None:
-                    logger.warning(f"{ip} had no alpha2 code, setting country name to '00'")
-                    asn_alpha2: str = '00'
-                    conn.execute(text(f'''update {SOURCES} SET country = '{asn_alpha2}' WHERE SOURCE = '{ip}';'''))
-                    continue
+            elif result['asn_country_code'].islower():
+                asn_alpha2: str = asn_alpha2.upper()
+                logger.warning(f'RDAP responded with lowercase country for {ip}, should be upper')
 
-                elif result['asn_country_code'].islower():
-                    asn_alpha2: str = asn_alpha2.upper()
-                    logger.warning(f'RDAP responded with lowercase country for {ip}, should be upper')
+            else:
+                asn_alpha2 = result['asn_country_code']
+                country_name: Optional[str] = COUNTRIES.get(asn_alpha2)
 
-                else:
-                    asn_alpha2 = result['asn_country_code']
-                    country_name: Optional[str] = COUNTRIES.get(asn_alpha2)
-
-                try:
-                    conn.execute(text(f'''UPDATE `{my_secrets.dbname}`.`{SOURCES}`
-                            SET
-                                `COUNTRY` = '{country_name}',
-                                `ALPHA2` = '{asn_alpha2}',
-                                `DESCRIPTION` = '{asn_description}'
-                            WHERE `SOURCE` = '{ip}';'''
+            try:
+                conn.execute(text(f'''UPDATE `{my_secrets.dbname}`.`{SOURCES}`
+                        SET
+                            `COUNTRY` = '{country_name}',
+                            `ALPHA2` = '{asn_alpha2}',
+                            `DESCRIPTION` = '{asn_description}'
+                        WHERE `SOURCE` = '{ip}';'''
                                   ))
-                except exc.ProgrammingError as e:
-                    logger.error(e)
+            except exc.ProgrammingError as e:
+                logger.error(e)
 
-    logger.info(f"SOURCES table: {len(no_country)-errors} updated with country names and ASN description. {errors} errors encountered")
+    logger.info(
+        f"SOURCES table: {len(no_country) - errors} updated with country names and ASN description. {errors} errors encountered")
