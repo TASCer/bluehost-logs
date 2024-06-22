9bd6f10918af765186e5e86e0f864c9c4b8a3ea4
TASCer <todd@tascs.net>
2024-06-22 11:05:34 -0700

    cleaned

--- assets/sample_my_secrets.py
+++ assets/sample_my_secrets.py
@@ -1,10 +1,9 @@
 # HOST IP ADXs
 bh_ip = "x.x.x.x"  # your remote server's ip
-home_ip = "x.x.x.x"
+home_ip = "x.x.x.x" # your home ip
 # SECURE ACCESS TO FILES ON ISP SERVER
 user = "your_user"
 password = "your_password"
-key_path = r"C:\path\to\your\private_keyfile"
 # ACCESS TO LOCAL BLUEHOST-WEBLOGS DB
 dbuser = "your_dbuser"
 dbpass = "your_dbpassword"
