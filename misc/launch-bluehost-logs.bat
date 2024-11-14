bd7d7b23406ab700e8e2d22c17287b571a42e49e
TASCer <todd@tascs.net>
2024-11-14 10:54:27 -0700

    new virtual env

--- misc/launch-bluehost-logs.bat
+++ misc/launch-bluehost-logs.bat
@@ -1,5 +1,5 @@
 cd /D "D:\PycharmProjects\bluehost-logs\src"
 echo %cd%
-call conda activate py10_BHlogs
+call conda activate bluehost-logs311
 python.exe "main.py"
 if NOT ["%errorlevel%"] == ["0"] pause
